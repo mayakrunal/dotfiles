8cdc243222f9d7923f0ebbfa2a7f9d3113a59f3e
mayakrunal <maya.krunal@gmail.com>
2023-03-12 18:48:34 +0800

    update qtile config

--- ken/scripts/10.1-qtile-setup.sh
+++ ken/scripts/10.1-qtile-setup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 #install
-sudo pacman -S qtile nitrogen python-psutils python-dbus-next
+yay -S qtile-git qtile-extras picom-git nitrogen python-psutils python-dbus-next
 
 yay -S rofi volumeicon brightnessctl pavucontrol blueman
